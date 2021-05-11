3f4053b8f3ee74e62920e4e68ffb575b63b95bd9
rosemaryto <rosemary_to@ymail.com>
2021-05-11 11:25:13 -0700

    begin

---
+++ js/oop-practice.js
@@ -0,0 +1,21 @@
+// STEP 1
+function Cat (breed) {};
+var Dog = function (breed) {};
+
+// STEP 2
+var persian = new Cat("Persian");
+var shibaInu = new Dog("ShibaInu");
+
+// STEP 3
+
+// STEP 4
+
+// STEP 5
+
+// STEP 6
+
+// STEP 7
+
+// STEP 8
+
+// STEP 9
