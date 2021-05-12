a0c065c214589e1c13171d5023000a65d008ef12
rosemaryto <rosemary_to@ymail.com>
2021-05-11 17:12:27 -0700

    step6

--- js/oop-practice.js
+++ js/oop-practice.js
@@ -7,16 +7,48 @@
 // var shibaInu = new Dog("ShibaInu");
 
 // STEP 3
-var Animal = function () {
-    "use strict";
-};
-window.console.log("The Animal has been created");
+// var Animal = function () {
+//     "use strict";
+//     window.console.log("The Animal has been created");
+// };
+// var dog = new Animal();
+
 
 // STEP 4
+// var Animal = function (bird) {
+//     "use strict";
+//     this.bird = bird;
+//     window.console.log("The animal is a " + this.bird);
+// };
+// var birdType = new Animal("HummingBird");
 
 // STEP 5
+// var Animal = function (type, breed, color, height, length) {
+//     "use strict";
+//     this.type = type;
+//     this.breed = breed;
+//     this.color = color;
+//     this.height = height;
+//     this.length = length;
+//     window.console.log("The type of animal is " + this.type + ". The breed of " + this.breed + " is " + this.color + " with a height of " + this.height + " & length of " + this.length);
+// };
+// var HummingBird = new Animal ("Bird", "Humming Bird", "blue", "3 inches", "4 inches");
 
 // STEP 6
+// var Animal = function (type, breed, color, height, length) {
+//     "use strict";
+//     this.type = type;
+//     this.breed = breed;
+//     this.color = color;
+//     this.height = height;
+//     this.length = length;
+// };
+// var HummingBird = new Animal ("Bird", "Humming Bird", "blue", "3 inches", "4 inches");
+// for (var animal in HummingBird) {
+//     window.console.log(animal);
+// }
+
+
 
 // STEP 7
 
