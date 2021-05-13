d621bebd9c5e2fb82e664def4f20cb7dc05d91c0
rosemaryto <rosemary_to@ymail.com>
2021-05-13 06:01:05 -0700

    recipe

---
+++ js/recipe-card.js
@@ -0,0 +1,8 @@
+var Recipe = {
+    title: "Guacamole",
+    servings: 4,
+};
+var ingredients = ["3 avacados", "1 lime", "1 Teaspoon Salt", "1/2 Cup Onion", "3 Tablespoons Cilantro", "2 Diced Tomatoes", "1 Teaspoon Garlic", "1 Pinch Ground Pepper"];
+window.console.log(Recipe.title);
+window.console.log("Serves: " + Recipe.servings);
+window.console.log("Ingredients: " + ingredients.toString());
